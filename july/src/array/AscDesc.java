e77f71959da495ae0ece5bf082def8c90517b89b
jitsn <119994767+jitsn@users.noreply.github.com>
2022-12-07 09:41:07 +0530

    Update AscDesc.java
    array changed

--- july/src/array/AscDesc.java
+++ july/src/array/AscDesc.java
@@ -4,7 +4,7 @@ import java.util.Arrays;
 
 public class AscDesc {
 public static void main(String[] args) {
-	int a[]= {10,30,90,56,34,52,5434,53,534};
+	int a[]= {100,300,900,560,340,520,543,537,568};
 	System.out.println("-----------to print aray values-------------");
 	for(int i=0;i<=a.length-1;i++) {
 		System.out.println(a[i]);
